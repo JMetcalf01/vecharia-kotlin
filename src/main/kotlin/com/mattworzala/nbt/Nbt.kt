2a4b254ded0f5149e56bb39374ac382b0657965d
Matt Worzala <mattheworzala@gmail.com>
2019-12-17 01:28:29 -0500

    very little testing was done, but the nbt code seems to be working.

--- src/main/kotlin/com/mattworzala/nbt/Nbt.kt
+++ src/main/kotlin/com/mattworzala/nbt/Nbt.kt
@@ -1,15 +1,27 @@
 package com.mattworzala.nbt
 
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
+import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
 object Nbt {
-    fun readCompound(file: String): DataCompound {
-        return readCompound(Paths.get(".").resolve(file))
-    }
+    fun readCompound(file: String): DataCompound = readCompound(Paths.get(".").resolve(file))
 
     fun readCompound(path: Path): DataCompound {
-//        with(val oStream = ObjectInputStream())
-        return DataCompound()
+        Files.newInputStream(path).use { fileInput ->
+            ObjectInputStream(fileInput).use {
+                return it.readObject() as DataCompound
+            }
+        }
+    }
+
+    fun writeCompound(data: DataCompound, file: String) = writeCompound(data, Paths.get(".").resolve(file))
+
+    fun writeCompound(data: DataCompound, path: Path) {
+        Files.newOutputStream(path).use { fileOutput ->
+            ObjectOutputStream(fileOutput).use { it.writeObject(data) }
+        }
     }
 }
