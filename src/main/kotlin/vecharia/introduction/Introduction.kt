4e768ffeb162b0f92c708e8c2ca6cce0c6647841
JonathanMetcalf <jonathan.e.metcalf@gmail.com>
2019-12-21 00:41:26 -0500

    Introduction is finished, but minor reworkings will happen soon (tm)

--- src/main/kotlin/vecharia/introduction/Introduction.kt
+++ src/main/kotlin/vecharia/introduction/Introduction.kt
@@ -46,8 +46,8 @@ class Introduction(val game: Vecharia) {
         getName(Player.Builder()).then { builder ->
             getRace(builder).then { builder1 ->
                 getClass(builder1).then { builder2 ->
-                    confirmClass(builder2).then {
-                        resolve(it.build())
+                    confirmClass(builder2).then { finalBuilder ->
+                        resolve(finalBuilder.build())
                     }
                 }
             }
@@ -97,6 +97,7 @@ class Introduction(val game: Vecharia) {
                         game.printer += "Zloridge is under the rule of King Henry IV."
                         game.printer += "The day to become a full-fledged adventurer has come!"
                         game.printer += "Your pay is meager, but you have managed to save up 1000 gold coins for your quest. [TODO WHEN INVENTORY IS IMPLEMENTED]"
+                        // add coins to builder.inventory
                         game.printer += "If you keep forgetting basic stuff like that you're a human, maybe you should be sleeping more..."
                         game.printer += Text(
                             "You dismiss that thought from your mind and get changed, find something to eat, and leave your hut.",
@@ -122,6 +123,7 @@ class Introduction(val game: Vecharia) {
                         game.printer += "Your leader is the proud King Meldal."
                         game.printer += "Today you will finally choose your quest!"
                         game.printer += "Your pay is not much, but you have managed to save up 1250 gold coins for your quest. [TODO WHEN INVENTORY IS IMPLEMENTED]"
+                        // add coins to builder.inventory
                         game.printer += "How could you forget you're a dwarf?"
                         game.printer += Text(
                             "You climb out of bed, struggling to reach the floor. Why are these beds so damn high?",
@@ -287,7 +289,7 @@ class Introduction(val game: Vecharia) {
                         2 -> {
                             builder.pclass = Player.Class.MAGIC
                             game.printer.clear()
-                            game.printer.waiting("A sorcerer? Alright! Spell me up some fuckin' beer!")
+                            game.printer.waiting("\"A sorcerer? Alright! Spell me up some fuckin' beer!\"")
                                 .then {
                                     resolve(builder)
                                 }
@@ -460,7 +462,18 @@ class Introduction(val game: Vecharia) {
      */
     private fun wizardHumanText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
         game.printer.clear()
-        game.printer += ""
+        game.printer += "\"We could use a new wizard. I'm glad you chose to become one.\""
+        game.printer.waiting("\"We'll give you the standard oak staff to begin with, it should get the job done for now. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add the oak staff to builder.inventory
+                // todo figure out mana as well
+                game.printer.waiting("\"${builder.name}, you may leave now. Good luck on your quest.")
+                    .then {
+                        game.printer += "You exit the castle carrying your new oak staff."
+                        resolve(builder)
+                    }
+            }
+
     }
 
     /**
@@ -486,20 +499,341 @@ class Introduction(val game: Vecharia) {
     /**
      * The meeting about the class for the elf.
      *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
      * @param builder the builder instance
      * @return a promise of the player builder
      */
     private fun elfMeeting(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "You make your way into your mentor's house."
+        game.printer += "Your mentor is upstairs at their desk where they normally are."
+        game.printer += "They gracefully turn to face you."
+        game.printer += "\"Greetings, ${builder.name}.\""
+        Menu.basic(
+            game,
+            "\"So, you've decided to become a ${getElfJob(builder)}. Are you sure of that choice?\"",
+            "Yes",
+            "No"
+        ).then { option ->
+            when (option) {
+                0 -> {
+                    when (builder.pclass) {
+                        Player.Class.MELEE -> duelistElfText(builder).then { resolve(it) }
+                        Player.Class.RANGED -> archerElfText(builder).then { resolve(it) }
+                        Player.Class.MAGIC -> mageElfText(builder).then { resolve(it) }
+                    }
+                }
+                1 -> {
+                    recheckClassElf(builder)
+                }
+            }
+        }
+    }
 
+    /**
+     * Rechecks the class if the user is unsure of what class they want to be.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     */
+    private fun recheckClassElf(builder: Player.Builder) {
+        Menu.basic(game, "\"What do you wish to be, then?\"", "Duelist", "Archer", "Mage").then { option ->
+            when (option) {
+                0 -> {
+                    builder.pclass = Player.Class.MELEE
+                    duelistElfText(builder)
+                }
+                1 -> {
+                    builder.pclass = Player.Class.RANGED
+                    archerElfText(builder)
+                }
+                2 -> {
+                    builder.pclass = Player.Class.MAGIC
+                    mageElfText(builder)
+                }
+            }
+        }
+    }
+
+    /**
+     * The final meeting with the mentor as a duelist.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun duelistElfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"I agree you as a duelist would be best.\""
+        game.printer += "\"Your rapier skills are already unparalleled among those your age.\""
+        game.printer += "\"As is customary, you shall take my rapier for your quest.\""
+        game.printer.waiting("They deliver their oldest, yet still perfectly maintained, rapier into your waiting hands. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add rapier to builder.inventory
+                game.printer += "\"Good luck on whatever quest you choose, young one.\""
+                game.printer.waiting("\"To help you out, I shall give you 750 gold coins. Spend them wisely.\" [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                    .then {
+                        // Add coins to builder.inventory
+                        game.printer += "You step outside the house with your rapier in a scabbard on your hip."
+                        resolve(builder)
+                    }
+            }
+    }
+
+    /**
+     * The final meeting with the mentor as an archer.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun archerElfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"I agree; archery would be best for you.\""
+        game.printer += "\"Your marksmanship is unparalleled among those your age.\""
+        game.printer += "\"As is customary, you shall take my bow for your quest.\""
+        game.printer.waiting("Your mentor hands you their oldest, yet still perfectly maintained, longbow into your waiting hands. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add longbow to builder.inventory
+                game.printer.waiting("They hand you a quiver filled with arrows to go along with it. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                    .then {
+                        // add arrows to builder.inventory
+                        game.printer += "\"Take this dagger as well.\""
+                        game.printer.waiting("Your mentor gives you a small, sharp dagger. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                            .then {
+                                // add dagger to builder.inventory
+                                game.printer += "\"Good luck on whatever quest you choose, young one.\""
+                                game.printer.waiting("\"To help you out, I shall give you 750 gold coins. Spend them wisely.\" [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                                    .then {
+                                        // add coins to builder.inventory
+                                        game.printer += "You step outside the house with your longbow slung over your shoulder."
+                                        resolve(builder)
+                                    }
+                            }
+                    }
+            }
+
+    }
+
+    /**
+     * The final meeting with the mentor as a mage.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun mageElfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"I agree that you would be best as a mage.\""
+        game.printer += "\"Your aptitude for spells is unparalleled among those your age.\""
+        game.printer += "\"As is customary, you shall take my staff for your quest.\""
+        game.printer.waiting("Your mentor hands you their oldest, yet perfectly maintained oak staff into your hands [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add the staff to builder.inventory
+                game.printer += "\"Best of luck on wherever your fate leads you, young one.\""
+                game.printer.waiting("Before you go, however, I shall give you 750 gold coins to help you out. Spend them well.\" [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                    .then {
+                        // add coins to builder.inventory
+                        game.printer += "You step outside the house with your staff in hand."
+                        resolve(builder)
+                    }
+            }
+    }
+
+    /**
+     * Returns the elf job name for whatever class they are.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @throws IllegalStateException if the job is still somehow null
+     * @param builder the builder object
+     * @return a string of the job name
+     */
+    private fun getElfJob(builder: Player.Builder): String {
+        return when (builder.pclass) {
+            Player.Class.MELEE -> "duelist"
+            Player.Class.RANGED -> "archer"
+            Player.Class.MAGIC -> "mage"
+            null -> throw IllegalStateException()
+        }
     }
 
     /**
      * The meeting about the class for the dwarf.
      *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
      * @param builder the builder instance
      * @return a promise of the player builder
      */
     private fun dwarfMeeting(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "You confidently stroll into the Great Hall."
+        game.printer += "The king is drinking ale at a table and gestures to sit down next to him."
+        game.printer.waiting("\"Oi, ${builder.name}! I'm proud of yah.\"").then {
+            Menu.basic(
+                game,
+                "\"My right-hand man right here tells me that yah wanna become a ${getDwarfJob(builder)}. Yah sure about that?\"",
+                "Yep",
+                "Nah"
+            ).then { option ->
+                when (option) {
+                    0 -> {
+                        when (builder.pclass) {
+                            Player.Class.MELEE -> fighterDwarfText(builder).then { resolve(it) }
+                            Player.Class.RANGED -> slingerDwarfText(builder).then { resolve(it) }
+                            Player.Class.MAGIC -> sorcererDwarfText(builder).then { resolve(it) }
+                        }
+                    }
+                    1 -> {
+                        recheckClassDwarf(builder)
+                    }
+                }
+            }
+        }
+    }
 
+    /**
+     * Rechecks the class if the user is unsure of what class they want to be.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     */
+    private fun recheckClassDwarf(builder: Player.Builder) {
+        Menu.basic(game, "\"Whatcha gonna be then?\"", "Fighter", "Slinger", "Sorcerer").then { option ->
+            when (option) {
+                0 -> {
+                    builder.pclass = Player.Class.MELEE
+                    fighterDwarfText(builder)
+                }
+                1 -> {
+                    builder.pclass = Player.Class.RANGED
+                    slingerDwarfText(builder)
+                }
+                2 -> {
+                    builder.pclass = Player.Class.MAGIC
+                    sorcererDwarfText(builder)
+                }
+            }
+        }
+    }
+
+    /**
+     * The final meeting with the king as a fighter.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun fighterDwarfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"You'll be a good 'un.\""
+        game.printer += "\"Take this axe.\""
+        game.printer.waiting("The king hands you a battleaxe. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add axe to builder.inventory
+                game.printer += "\"I want some peace and quiet now. Do whichever quest you're doing or don't come back at all.\""
+//                game.printer.delay(3000)
+                game.printer.waiting("\"Nah, I'm just kiddin'. Good luck!\"")
+                    .then {
+                        game.printer += "You step out of the Great Hall with your battleaxe on your back."
+                        resolve(builder)
+                    }
+            }
+    }
+
+    /**
+     * The final meeting with the mentor as a slinger.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun slingerDwarfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"A slinger? I didn't peg yah as a coward!\""
+//        game.printer.delay(3000)
+        game.printer += "\"I'm just joshin' with yah! A slinger would be a good job for yah.\""
+        game.printer += "\"I'll give you a nice sling, don'tcha worry.\""
+        game.printer.waiting("The king hands you a sling and a carton of rounded balls of lead. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add sling and lead balls to builder.inventory
+                game.printer += "You lift it. \"Damn, that's heavy,\" you think to yourself."
+                game.printer += "\"Take this dagger as well.\""
+                game.printer.waiting("The king hands you a small, sharp dagger. [TODO WHEN INVENTORY IS IMPLEMENTED]")
+                    .then {
+                        // add small dagger to builder.inventory
+                        game.printer += "\"I want some peace and quiet now. Do that quest or don't come back at all.\""
+//                        game.printer.delay(3000)
+                        game.printer.waiting("\"Nah, I'm just kiddin'. Good luck on your quest!\"").then {
+                            game.printer += "You step out of the Great Hall with your sling and lead balls in your pocket."
+                            resolve(builder)
+                        }
+                    }
+            }
+    }
+
+    /**
+     * The final meeting with the mentor as a sorcerer.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @param builder the builder instance
+     * @return a promise of the player builder
+     */
+    private fun sorcererDwarfText(builder: Player.Builder) = Promise<Player.Builder> { resolve ->
+        game.printer.clear()
+        game.printer += "\"A sorcerer, eh? You better be useful to me!\""
+        game.printer += "\"Take this cool stick I found on the ground on my daily walk yesterday.\""
+        game.printer += "\"They always say that it's not the quality of the stick, it's how you use it!\""
+        game.printer.waiting("The king hands you the cool stick. Thanks a lot... [TODO WHEN INVENTORY IS IMPLEMENTED]")
+            .then {
+                // add the cool stick to builder.inventory
+                game.printer += "\"I want some peace and quiet now. Do that quest or don't come back at all.\""
+//            game.printer.delay(3000)
+                game.printer.waiting("\"Nah, I'm just kiddin'. Good luck on your quest!\"")
+                    .then {
+                        game.printer += "You step out of the Great Hall with your sling and lead balls in your pocket."
+                        resolve(builder)
+                    }
+            }
+    }
+
+    /**
+     * Returns the dwarf job name for whatever class they are.
+     *
+     * @author Jonathan Metcalf
+     * @since 1.3
+     *
+     * @throws IllegalStateException if the job is still somehow null
+     * @param builder the builder object
+     * @return a string of the job name
+     */
+    private fun getDwarfJob(builder: Player.Builder): String {
+        return when (builder.pclass) {
+            Player.Class.MELEE -> "fighter"
+            Player.Class.RANGED -> "slinger"
+            Player.Class.MAGIC -> "sorcerer"
+            null -> throw IllegalStateException()
+        }
     }
 }
